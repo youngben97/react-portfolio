eecfe9e8aca30cc9a2feec5980030d871b533a27
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-08 23:13:48 -0700

    pages

---
+++ src/pages/ResumePage.jsx
@@ -0,0 +1,23 @@
+//static about me page
+export default function AboutPage() {
+    return (
+        <div className='container pt-4'>
+            <div className='row p-2'>
+                <div className='col-lg-4'>
+                    <p>
+                        Hello! I am a web developer based in Salt Lake City, UT.
+                        I have experience working with HTML, CSS, JS, SQL/Sequelize, MongoDB/Mongoose,
+                        Express, Node, and most recently, React.
+
+                        In my free time I like to read, cook, and get involved
+                        in my local theatre community.
+                    </p>
+                </div>
+            </div>
+            <div className='row p-2'>
+                <img className='col-lg-4'></img>
+                //add an image
+            </div>
+        </div>
+    )
+}
