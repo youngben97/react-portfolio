c48b43c67b328b9c26984d717966ee8dedb5cc72
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-08 23:12:22 -0700

    main. react routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+
+// import React from 'react' - do you need this?
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from './App.jsx';
+import ErrorPage from './pages/ErrorPage';
+import ContactPage from './pages/ContactPage';
+import PortfolioPage from './pages/PortfolioPage';
+import AboutPage from './pages/AboutPage';
+import ResumePage from './pages/ResumePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutPage />,
+      },
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
