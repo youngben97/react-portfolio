f67e9648074fc4a11db35db469f883674984e0ab
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-09 20:54:26 -0700

    changed link to navlink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,23 @@
 // Bringing in the required import from 'react-router-dom'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Navbar from './UI/Navbar';
 
 export default function Nav() {
     return (
         <Navbar
           links={[
-            <Link key={1} className='nav-link text-light' to='/'>
+            <NavLink key={1} className='nav-link text-light' to='/'>
                 About
-            </Link>,
-            <Link key={2} className='nav-link text-light' to='/portfolio'>
+            </NavLink>,
+            <NavLink key={2} className='nav-link text-light' to='/portfolio'>
                 Portfolio
-            </Link>,
-            <Link key={3} className='nav-link text-light' to='/resume'>
+            </NavLink>,
+            <NavLink key={3} className='nav-link text-light' to='/resume'>
                 Resume
-            </Link>,
-            <Link key={4} className='nav-link text-light' to='/contact'>
+            </NavLink>,
+            <NavLink key={4} className='nav-link text-light' to='/contact'>
                 Contact
-            </Link>
+            </NavLink>
           ]} />
     );
 }
